1cba986b95be7b04eed360b4c7b1a368138d001e
Lluís Revilla <lluis.revilla@gmail.com>
2016-06-29 12:57:54 +0200

    Remove unwanted filtering (for the moment)

--- classify_sets.R
+++ classify_sets.R
@@ -67,55 +67,55 @@ cd4.58 <- cd4[, as.numeric(lapply(samples.a, grep, colnames(cd4), fixed = T))]
 # Load the manually downloaded expression set of the E-MTAB-157
 # load(file.path(data.folder, "E-MTAB-157.eSet.r"))
 # Or load from the online file
-source("http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-157/E-MTAB-157.eSet.r")
-sle <- study
-clinic.sle <- pData(sle)
-clinic.sle <- as.data.frame(apply(clinic.sle, 2, as.factor))
-SLE <- subset(clinic.sle, 
-              Factor.Value..DISEASESTATE. == "Systemic lupus erythematosus")
-
-
-files.sample <- function(sample, data){
-  # Function to obtain the names of the files of paired samples by swapping
-  # Given a pData(NChannel) and a sample name then creates the names of files
-  sub <- subset(data, Source.Name.Cy3 == sample | Source.Name.Cy5 == sample, 
-                select = Scan.Name)
-  s <- grep("D10", as.character(sub$Scan.Name), ignore.case = TRUE,
-            invert = TRUE, value = TRUE)
-  return(s)
-}
-
-files.paired <- function(files){
-  # Return the two possibilites of the name they can be labelled with
-  if (length(files) > 2) {
-    stop("The files must be just two!")
-  }
-  if (length(files) == 0) {
-    return(c("", ""))
-  }
-  a <- paste0(files, collapse = ".")
-  b <- paste0(files[2:1], collapse = ".")
-  return(c(a, b))
-}
-
-samples <- as.character(unique(SLE$Source.Name.Cy3))
-samples <- samples[samples != "Ref"]
-files.samples <- sapply(samples, files.sample, data = SLE)
-files.paired <- sapply(files.samples, files.paired)
-files.paired <- as.character(files.paired)
-files.paired <- files.paired[files.paired != ""]
-
-# Files processed
-sle.processed <- basename(levels(SLE$Comment..Derived.ArrayExpress.FTP.file.))
-proc.path <- file.path(data.folder, sle.processed)
-
-u.files <- unzip(proc.path, exdir = data.folder)
-sle.p <- read.delim(u.files)
-sle.p <- sle.p[-1, ] # Remove the log2 row
-sle.p2 <- sle.p[, colnames(sle.p) %in% files.paired]
-
-########################### IBD ###########################
-
-sdrf.331 <- grep("331", sdrf, value = T)
-ibd <- read.delim(sdrf.331)
-ibd <- as.data.frame(apply(ibd, 2, as.factor))
+# source("http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-157/E-MTAB-157.eSet.r")
+# sle <- study
+# clinic.sle <- pData(sle)
+# clinic.sle <- as.data.frame(apply(clinic.sle, 2, as.factor))
+# SLE <- subset(clinic.sle, 
+#               Factor.Value..DISEASESTATE. == "Systemic lupus erythematosus")
+# 
+# 
+# files.sample <- function(sample, data){
+#   # Function to obtain the names of the files of paired samples by swapping
+#   # Given a pData(NChannel) and a sample name then creates the names of files
+#   sub <- subset(data, Source.Name.Cy3 == sample | Source.Name.Cy5 == sample, 
+#                 select = Scan.Name)
+#   s <- grep("D10", as.character(sub$Scan.Name), ignore.case = TRUE,
+#             invert = TRUE, value = TRUE)
+#   return(s)
+# }
+# 
+# files.paired <- function(files){
+#   # Return the two possibilites of the name they can be labelled with
+#   if (length(files) > 2) {
+#     stop("The files must be just two!")
+#   }
+#   if (length(files) == 0) {
+#     return(c("", ""))
+#   }
+#   a <- paste0(files, collapse = ".")
+#   b <- paste0(files[2:1], collapse = ".")
+#   return(c(a, b))
+# }
+# 
+# samples <- as.character(unique(SLE$Source.Name.Cy3))
+# samples <- samples[samples != "Ref"]
+# files.samples <- sapply(samples, files.sample, data = SLE)
+# files.paired <- sapply(files.samples, files.paired)
+# files.paired <- as.character(files.paired)
+# files.paired <- files.paired[files.paired != ""]
+# 
+# # Files processed
+# sle.processed <- basename(levels(SLE$Comment..Derived.ArrayExpress.FTP.file.))
+# proc.path <- file.path(data.folder, sle.processed)
+# 
+# u.files <- unzip(proc.path, exdir = data.folder)
+# sle.p <- read.delim(u.files)
+# sle.p <- sle.p[-1, ] # Remove the log2 row
+# sle.p2 <- sle.p[, colnames(sle.p) %in% files.paired]
+# 
+# ########################### IBD ###########################
+# 
+# sdrf.331 <- grep("331", sdrf, value = T)
+# ibd <- read.delim(sdrf.331)
+# ibd <- as.data.frame(apply(ibd, 2, as.factor))
