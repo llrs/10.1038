cb637aa35150c090cda6647194fe3f67c7abecdb
Lluís Revilla <lluis.revilla@gmail.com>
2016-06-30 11:28:11 +0200

    Commenting the clinical part, and adding MAD plot

--- aav_wgcna.R
+++ aav_wgcna.R
@@ -27,17 +27,19 @@ rownames(cd8.t) <- colnames(cd8.44)
 # cd8.exp <- apply(cd8.t, 1:2, exp2)
 
 # Calculate the median absolute deviation values for all proves
-# cd4.mad <- apply(cd4.t, 2, mad, na.rm = T)
-# cd4.mad <- cd4.mad[!is.na(cd4.mad)]
-# cd8.mad <- apply(cd8.t, 2, mad, na.rm = T)
-# cd8.mad <- cd8.mad[!is.na(cd8.mad)]
-# plot(cd4.mad, 1 - rank(cd4.mad)/length(cd4.mad),
-# ylab = "% of points over", main = "CD4)
-# plot(cd8.mad, 1 - rank(cd8.mad)/length(cd8.mad),
-#      ylab = "% of points over", main = "CD8")
-
+cd4.mad <- apply(cd4.t, 2, mad, na.rm = T)
+cd4.mad <- cd4.mad[!is.na(cd4.mad)]
+cd8.mad <- apply(cd8.t, 2, mad, na.rm = T)
+cd8.mad <- cd8.mad[!is.na(cd8.mad)]
+png("MAD_filtering.png", width = 2000, height = 2000)
+plot(cd4.mad, 1 - rank(cd4.mad)/length(cd4.mad),
+  ylab = "% of points over", main = "MAD score", col = "blue")
+points(cd8.mad, 1 - rank(cd8.mad)/length(cd8.mad),
+     ylab = "% of points over", col = "red")
+legend("topright", legend = c("CD8", "CD4"),
+       fill = c("red", "blue"))
 # Lacks of the filtering using inflexion point of the ranked list of mad.
-
+dev.off()
 exp_conditions <- list("CD4" = cd4.t, "CD8" = cd8.t)
 
 n <- 0
@@ -101,9 +103,9 @@ for (exp in exp_conditions) {
   dev.off()
 
   if (is.null(sft$powerEstimate)) {
-    stop("RsquaredCut option of the pickSoftThreshold to hight.\n")
+    stop("RsquaredCut option of the pickSoftThreshold too height.\n")
   }
-  cat(paste0("Value of power used is ", sft$powerEstimate, "\n"))
+  cat(paste0("Value of power used is ", sft$powerEstimate, ".\n"))
 
   # Do an automatic blocking
   net <- blockwiseModules(exp,
@@ -113,48 +115,60 @@ for (exp in exp_conditions) {
             saveTOMs = TRUE,
             saveTOMFileBase = paste0("samples_", nam, "_TOM"),
             verbose = 1)
-
+  print("\n")
   table(net$colors)
+  print("\n")
+  
+  pdf(paste0("modules_", nam, ".pdf"))
+  # open a graphics window
+  sizeGrWindow(12, 9)
 
-
+  # Plot the dendrogram and the module colors underneath
+  plotDendroAndColors(net$dendrograms[[1]], 
+                      net$colors[net$blockGenes[[1]]],
+                      "Module colors",
+                      dendroLabels = FALSE, hang = 0.03,
+                      addGuide = TRUE, guideHang = 0.05)
+  dev.off()
+  
   # Make the table of correlation between modules and clinical data
-  traits <- read.delim("fileinexistente.com")
-  MEs0 <- moduleEigengenes(exp, net$colors)
-  MEs <- orderMEs(MEs0$eigengenes)
-  moduleTraitCor <- cor(MEs, traits, use = "p")
-  moduleTraitPvalue <- corPvalueStudent(moduleTraitCor, ncol(exp))
-
-  # plotMat()
-  # plotModuleSignificance
-  barplot(as.matrix(MEs0$varExplained),
-          names = colnames(MEs0$eigengenes), las = 2)
-  boxplot(t(as.matrix(MEs0$varExplained)), main = "Explanation of the Eigengenes",
-          xlab = "Eigengenes", ylab = "% Explained", outline = FALSE)
-  stripchart(t(as.matrix(MEs0$varExplained)), add = TRUE, vertical = T,
-             pch = 21, bg = substring(colnames(MEs0$eigengenes), 3), method = "jitter")
-  sizeGrWindow(10,6)
-  # Will display correlations and their p-values
-  textMatrix <-  paste(ifelse(
-    moduleTraitPvalue <= 0.1 & abs(moduleTraitCor) >= 0.3, "* ", ""),
-    signif(moduleTraitCor, 2),
-    " (",
-    signif(moduleTraitPvalue, 1), ")",
-    sep = "")
-  # Force to get the structure of teh matrix
-  dim(textMatrix) <-  dim(moduleTraitCor)
-  par(mar = c(3, 10, 4, 2));
-
-  # Display the correlation values within a heatmap plot
-  labeledHeatmap(Matrix = moduleTraitCor,
-                 xLabels = "Shedding",
-                 yLabels = names(MEs),
-                 ySymbols = names(MEs),
-                 colorLabels = F,
-                 colors = blueWhiteRed(50),
-                 textMatrix = textMatrix,
-                 setStdMargins = F,
-                 cex.text = 0.5,
-                 zlim = c(-1,1),
-                 main = "Module-trait relationships",
-                 width = 1)
+  # traits <- read.delim("fileinexistente.com")
+  # MEs0 <- moduleEigengenes(exp, net$colors)
+  # MEs <- orderMEs(MEs0$eigengenes)
+  # moduleTraitCor <- cor(MEs, traits, use = "p")
+  # moduleTraitPvalue <- corPvalueStudent(moduleTraitCor, ncol(exp))
+  # 
+  # # plotMat()
+  # # plotModuleSignificance
+  # barplot(as.matrix(MEs0$varExplained),
+  #         names = colnames(MEs0$eigengenes), las = 2)
+  # boxplot(t(as.matrix(MEs0$varExplained)), main = "Explanation of the Eigengenes",
+  #         xlab = "Eigengenes", ylab = "% Explained", outline = FALSE)
+  # stripchart(t(as.matrix(MEs0$varExplained)), add = TRUE, vertical = T,
+  #            pch = 21, bg = substring(colnames(MEs0$eigengenes), 3), method = "jitter")
+  # sizeGrWindow(10,6)
+  # # Will display correlations and their p-values
+  # textMatrix <-  paste(ifelse(
+  #   moduleTraitPvalue <= 0.1 & abs(moduleTraitCor) >= 0.3, "* ", ""),
+  #   signif(moduleTraitCor, 2),
+  #   " (",
+  #   signif(moduleTraitPvalue, 1), ")",
+  #   sep = "")
+  # # Force to get the structure of teh matrix
+  # dim(textMatrix) <-  dim(moduleTraitCor)
+  # par(mar = c(3, 10, 4, 2));
+  # 
+  # # Display the correlation values within a heatmap plot
+  # labeledHeatmap(Matrix = moduleTraitCor,
+  #                xLabels = "Shedding",
+  #                yLabels = names(MEs),
+  #                ySymbols = names(MEs),
+  #                colorLabels = F,
+  #                colors = blueWhiteRed(50),
+  #                textMatrix = textMatrix,
+  #                setStdMargins = F,
+  #                cex.text = 0.5,
+  #                zlim = c(-1,1),
+  #                main = "Module-trait relationships",
+  #                width = 1)
 }
