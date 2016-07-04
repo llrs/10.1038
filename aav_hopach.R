560403d3fc101e7e6f216ecd1cfd8153ae1f7a5f
Lluís Revilla <lluis.revilla@gmail.com>
2016-07-04 11:15:25 +0200

    Running the hopach analysis on the CD8 dataset

--- aav_hopach.R
+++ aav_hopach.R
@@ -4,3 +4,13 @@ source("classify_sets.R")
 
 library("hopach")
 
+cd4.44 <- apply(cd4.44, 1:2, as.numeric)
+gene.dist <- distancematrix(cd4.44, "cosangle", verbose = TRUE)
+gene.hobj <- hopach(cd4.44, dmat = gene.dist)
+
+print(table(gene.hobj$clust$labels))
+
+png("cluster_hopach_CD4.png", width = height = 1500)
+dplot(gene.dist,gene.hobj, ord = "final",
+      main = "Cluster hopach CD4", showclusters = F)
+dev.off()
